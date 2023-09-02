5df0caa5d45d181a4c1f08000e13fb0b561a236a
NightDevilPT <pawankumartadagsingh@gmail.com>
2023-09-02 09:06:50 +0530

    model updation

--- models/userModel.js
+++ models/userModel.js
@@ -1,84 +1,83 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    avtar:{
-        type:String,
-        default:''
-    },
-    firstname:{
-        type:String,
-        required:[true,'Firstname is required'],
-    },
-    lastname:{
-        type:String,
-        required:[true,'Lastname is required'],
-    },
-    gender:{
-        type:String,
-        required:[true,'Gender is required'],
-    },
-    email:{
-        type:String,
-        required:[true,'Email is required'],
-    },
-    password:{
-        type:String,
-        required:[true,'Password is required'],
-    },
-    token:{
-        type:String||null,
-        default:null,
-    },
-    verified:{
-        type:Boolean,
-        default:false,
-    },
-    facebook:{
-        type:String,
-        default:""
-    },
-    instagram:{
-        type:String,
-        default:""
-    },
-    youtube:{
-        type:String,
-        default:""
-    },
-    thread:{
-        type:String,
-        default:""
-    },
-    github:{
-        type:String,
-        default:""
-    },
-    portfolio:{
-        type:String,
-        default:""
-    },
-    followers:{
-        type:Array,
-        default:[]
-    },
-    following:{
-        type:Array,
-        default:[]
-    },
-    blogs:{
-        type:Array,
-        default:[]
-    },
-    saved:{
-        type:Array,
-        default:[]
-    },
-    liked:{
-        type:Array,
-        default:[]
-    },
+	avtar: {
+		type: String,
+		default: "",
+	},
+	firstname: {
+		type: String,
+		required: [true, "Firstname is required"],
+	},
+	lastname: {
+		type: String,
+		required: [true, "Lastname is required"],
+	},
+	gender: {
+		type: String,
+		default: "",
+	},
+	email: {
+		type: String,
+		required: [true, "Email is required"],
+	},
+	password: {
+		type: String,
+		required: [true, "Password is required"],
+	},
+	token: {
+		type: String || null,
+		default: null,
+	},
+	verified: {
+		type: Boolean,
+		default: false,
+	},
+	facebook: {
+		type: String,
+		default: "",
+	},
+	instagram: {
+		type: String,
+		default: "",
+	},
+	youtube: {
+		type: String,
+		default: "",
+	},
+	thread: {
+		type: String,
+		default: "",
+	},
+	github: {
+		type: String,
+		default: "",
+	},
+	portfolio: {
+		type: String,
+		default: "",
+	},
+	followers: {
+		type: Array,
+		default: [],
+	},
+	following: {
+		type: Array,
+		default: [],
+	},
+	blogs: {
+		type: Array,
+		default: [],
+	},
+	saved: {
+		type: Array,
+		default: [],
+	},
+	liked: {
+		type: Array,
+		default: [],
+	},
 });
 
-const userModel = mongoose.models.users || mongoose.model('users',userSchema);
+const userModel = mongoose.models.users || mongoose.model("users", userSchema);
 module.exports = userModel;
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,26 +1,33 @@
-const userRouter = require('express').Router();
+const userRouter = require("express").Router();
 // ------ Connection between MOngoDB Atlas
-const ConnectDB = require('../middleware/ConnectDB');
+const ConnectDB = require("../middleware/ConnectDB");
 // ------ Generate HashPassword Function
-const { GenerateHashPassword } = require('../libs/HashPassword');
+const { GenerateHashPassword } = require("../libs/HashPassword");
 // ------ Generate JWT Token
-const { GenerateToken } = require('../libs/JwtToken');
+const { GenerateToken } = require("../libs/JwtToken");
 // ------ MongoDB UserModel
-const userModel = require('../models/userModel');
+const userModel = require("../models/userModel");
 // ------ User controller
-const { AddNewUser, LoginUser, UpdateUserData, VerifyUserLink, GetUserProfile, GetAnotherUserProfile, UpdateSaved, UpdateFollowers } = require('../controllers/user');
-const { VerifyHeaderToken } = require('../middleware/middlewares');
-
+const {
+	AddNewUser,
+	LoginUser,
+	UpdateUserData,
+	VerifyUserLink,
+	GetUserProfile,
+	GetAnotherUserProfile,
+	UpdateSaved,
+	UpdateFollowers,
+} = require("../controllers/user");
+const { VerifyHeaderToken } = require("../middleware/middlewares");
 
 // ------- User Router Paths
-userRouter.get('/',GetAnotherUserProfile);
-userRouter.get('/profile',VerifyHeaderToken,GetUserProfile);
-userRouter.get('/login',LoginUser)
-userRouter.post('/create',AddNewUser);
-userRouter.put('/verify',VerifyUserLink);
-userRouter.put('/update',VerifyHeaderToken,UpdateUserData);
-userRouter.put('/updatesaved',VerifyHeaderToken,UpdateSaved);
-userRouter.put('/updatefollow',VerifyHeaderToken,UpdateFollowers);
-
+userRouter.get("/", GetAnotherUserProfile);
+userRouter.get("/profile", VerifyHeaderToken, GetUserProfile);
+userRouter.get("/login", LoginUser);
+userRouter.post("/create", AddNewUser);
+userRouter.put("/verify", VerifyUserLink);
+userRouter.put("/update", VerifyHeaderToken, UpdateUserData);
+userRouter.put("/updatesaved", VerifyHeaderToken, UpdateSaved);
+userRouter.put("/updatefollow", VerifyHeaderToken, UpdateFollowers);
 
 module.exports = userRouter;
